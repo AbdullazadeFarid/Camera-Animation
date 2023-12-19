b0c2cd4d4399f09d8bde88bce23d9f8399f4e26f
AbdullazadeFarid <abdullazadefarid@mail.ru>
2023-12-19 08:30:13 +0300

    .

--- js/main.js
+++ js/main.js
@@ -10,13 +10,13 @@ var audio = new Audio("./cameravoice.wav");
 
 click.addEventListener("click", function () {
 
-    flash.style.background = "white";
+    flash.style.background = "white";   // flash yandiqda ay reng elave olunacag
 
 
     audio.play();
 
     setTimeout(function () {
-        flash.style.background = "rgb(69, 69, 69)";
+        flash.style.background = "rgb(69, 69, 69)"; // 5 milli saniye sora o elave olunan flash legv olunub boz renge geri donecek
     }, 500);
 });
 
